a83b61a1c83f40f6707d93b011f910983d811c37
audioidiom <audioidiom@yandex.ru>
2023-12-22 19:57:44 +0300

    update script formats

--- scripts/infer.py
+++ scripts/infer.py
@@ -1,15 +1,19 @@
 import random
-import skops.io as skops
-import pandas as pd
+
 import numpy as np
+import pandas as pd
 import preprocessing as pp
+import skops.io as skops
 
 random.seed(42)
 np.random.seed(42)
 
 
 def infer():
-    df_test = pd.read_csv('https://raw.githubusercontent.com/Murcha1990/MLDS_ML_2022/main/Hometasks/HT1/cars_test.csv')
+    df_test = pd.read_csv(
+        """https://raw.githubusercontent.com/
+        Murcha1990/MLDS_ML_2022/main/Hometasks/HT1/cars_test.csv"""
+    )
 
     print("Test data shape: ", df_test.shape)
 
@@ -19,32 +23,31 @@ def infer():
     df_test = pp.max_power_clean_n_cast(df_test)
 
     # Столбец torque просто дропнем
-    df_test = df_test.drop(columns='torque')
+    df_test = df_test.drop(columns="torque")
 
     # Заполняем пустые значения медианами
-    df_test = pp.filling_na_with_medians(df_test, 'infer')
+    df_test = pp.filling_na_with_medians(df_test, "infer")
 
     # Кастуем к инту
-    df_test['engine'] = df_test['engine'].astype('int64')
-    df_test['seats'] = df_test['seats'].astype('int64')
+    df_test["engine"] = df_test["engine"].astype("int64")
+    df_test["seats"] = df_test["seats"].astype("int64")
 
     # Дроп ненужных столбцов
-    if 'selling_price' in df_test.columns:
-        X_test = df_test.drop(columns=['selling_price', 'name'])
+    if "selling_price" in df_test.columns:
+        X_test = df_test.drop(columns=["selling_price", "name"])
     else:
-        X_test = df_test.drop(columns=['name'])
+        X_test = df_test.drop(columns=["name"])
 
     # стандартизируем вещественные признаки
-    X_test_num_scaled = pp.normalize_numerical(X_test, 'infer')
+    X_test_num_scaled = pp.normalize_numerical(X_test, "infer")
 
     # кодируем категориальные признаки
-    X_test_cat_coded = pp.encoding_categorical(X_test, 'infer')
+    X_test_cat_coded = pp.encoding_categorical(X_test, "infer")
 
     # объединяем стандартизованный и закодированный элементы
-    X_test_preprocessed = X_test_num_scaled.merge(X_test_cat_coded,
-                                                how='inner',
-                                                left_index=True,
-                                                right_index=True)
+    X_test_preprocessed = X_test_num_scaled.merge(
+        X_test_cat_coded, how="inner", left_index=True, right_index=True
+    )
     X_test_preprocessed = X_test_preprocessed.to_numpy()
 
     # Импорт модели
@@ -53,7 +56,7 @@ def infer():
 
     # Получаем предсказания модели и сохраняем их
     y_pred = ridge_reg.predict(X_test_preprocessed)
-    pd.Series(y_pred).to_csv('y_pred.csv')
+    pd.Series(y_pred).to_csv("y_pred.csv")
 
 
 if __name__ == "__main__":
